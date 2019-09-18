672ce8cf2c89172d3bd709c15cdd33cdbcf30a1e
Egor <egor@opensrc.club>
2019-09-18 13:16:16 +0300

    reduce the size of the nav labels

--- make.sh
+++ make.sh
@@ -5,3 +5,6 @@ cp ../../resource/styles/steam.styles resource/styles/steam.styles
 
 cp ../../resource/layout/steamrootdialog.layout resource/layout/steamrootdialog.layout &&
 patch --binary -p2 <steamrootdialog.patch
+
+cp ../../resource/layout/uinavigatorpanel.layout resource/layout/uinavigatorpanel.layout &&
+patch --binary -p2 <uinavigatorpanel.patch
--- readme.txt
+++ readme.txt
@@ -1,2 +1,3 @@
 list of tweaks:
 - reduce the size of the bottom part of the frame
+- reduce the size of the nav labels
